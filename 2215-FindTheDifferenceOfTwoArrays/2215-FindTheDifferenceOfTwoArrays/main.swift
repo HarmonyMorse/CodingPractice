970a4444790822aea9d597fe9550cd6976c1d269
HarmonyMorse <harmonymorse@icloud.com>
2023-10-18 08:50:20 -0700

    Complete solution so tests pass.

--- 2215-FindTheDifferenceOfTwoArrays/2215-FindTheDifferenceOfTwoArrays/main.swift
+++ 2215-FindTheDifferenceOfTwoArrays/2215-FindTheDifferenceOfTwoArrays/main.swift
@@ -16,8 +16,17 @@ import Foundation
 
 class Solution {
     func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
-        var answer: [[Int]] = [[]]
-        
+        var answer: [[Int]] = [[],[]]
+        var nums1Set = Set(nums1)
+        var nums2Set = Set(nums2)
+        for num in nums2Set {
+            while nums1Set.contains(num) {
+                nums1Set.remove(num)
+                nums2Set.remove(num)
+            }
+        }
+        answer[0] = Array(nums1Set)
+        answer[1] = Array(nums2Set)
         return answer
     }
 }
@@ -28,13 +37,17 @@ let s = Solution()
 //Example 1:
 var nums1 = [1,2,3]
 var nums2 = [2,4,6]
-var output = [[1,3],[4,6]]
-assert(s.findDifference(nums1, nums2) == output)
+// All possible outputs for Example 1:
+var output = [[[1,3],[4,6]],
+              [[1,3],[6,4]],
+              [[3,1],[4,6]],
+              [[3,1],[6,4]]]
+assert(output.contains(s.findDifference(nums1, nums2)))
 
 //Example 2:
 nums1 = [1,2,3,3]
 nums2 = [1,1,2,2]
-output = [[3],[]]
-assert(s.findDifference(nums1, nums2) == output)
+var output2 = [[3],[]]
+assert(s.findDifference(nums1, nums2) == output2)
 
 print("Tests passed!")
