555f4cb0fa363be88d2e8277f838490a4eeed860
HarmonyMorse <harmonymorse@icloud.com>
2023-10-26 11:58:48 -0700

    Compile all work into extra file and put working solution in main.swift.

--- 338-CountingBits/338-CountingBits/main.swift
+++ 338-CountingBits/338-CountingBits/main.swift
@@ -10,21 +10,24 @@ import Foundation
 /*
  Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
  */
+
 class Solution {
-    func countBits(_ n: Int) -> [Int] {
+    func countBits(_ n: Int) -> [Int]  { // ~80ms, 19.5MB
         var ans: [Int] = Array(repeating: 0, count: n + 1)
         
         for i in 0...n {
-            var count = 0
+            // var count = 0
             
             let binaryString = String(i, radix: 2)
             let binaryArray = Array(binaryString)
             
-            for j in 0..<binaryArray.count {
-                count += Int(String(binaryArray[j]))!
-            }
+            // for j in 0..<binaryArray.count {
+            // count += Int(String(binaryArray[j]))!
+            // }
             
-            ans[i] = count
+            // ans[i] = count
+            
+            ans[i] = binaryArray.filter{$0 == "1"}.count
         }
         
         return ans
