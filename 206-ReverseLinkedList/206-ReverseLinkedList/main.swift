ff7ec1d74e5d02003beabf0100f30761d836be7c
HarmonyMorse <harmonymorse@icloud.com>
2023-10-30 09:09:41 -0700

    Create Xcode project for LeetCode 206 - Reverse Linked List.

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList/main.swift
@@ -0,0 +1,33 @@
+//
+//  main.swift
+//  206-ReverseLinkedList
+//
+//  Created by Harm on 10/27/23.
+//
+
+import Foundation
+
+
+// TODO: Goal
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Definition for singly-linked list:
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
+
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        return ListNode(0)
+    }
+}
+
+
+let s = Solution()
