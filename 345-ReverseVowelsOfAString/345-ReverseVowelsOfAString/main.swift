8fea64d8d777d806d1b8d064888c2816b395a773
HarmonyMorse <harmonythedev@gmail.com>
2024-03-06 20:11:45 -0800

    Add template for problem.

--- 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString/main.swift
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+class Solution {
+    func reverseVowels(_ s: String) -> String {
+        return s
+    }
+}
 
+let s = Solution()
+
+print("Running...")
+
+//Example 1:
+var inp = "hello"
+var output = "holle"
+var result = s.reverseVowels(inp)
+assert(result == output)
+
+//Example 2:
+inp = "leetcode"
+output = "leotcede"
+result = s.reverseVowels(inp)
+assert(result == output)
+
+//Example 3:
+inp = "epic"
+output = "icep"
+result = s.reverseVowels(inp)
+assert(result == output)
+
+print("Tests passed!")
